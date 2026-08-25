de44bd171e089b89696b70f1c2e0ae0fc05178a1
Maria <115981159+vcmeamaria@users.noreply.github.com>
2026-08-25 10:51:58 +0100

    Add reusable Selenium BaseTest class

---
+++ SeleniumCSharpDemo/Tests/BaseTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCSharpDemo.Tests;
+
+public class BaseTest
+{
+    protected IWebDriver Driver = null!;
+
+    [SetUp]
+    public void BaseSetup()
+    {
+        Driver = new ChromeDriver();
+        Driver.Manage().Window.Maximize();
+    }
+
+    [TearDown]
+    public void BaseTearDown()
+    {
+        Driver?.Quit();
+        Driver?.Dispose();
+    }
+}
---
+++ SeleniumCSharpDemo/Tests/BaseTestExampleTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace SeleniumCSharpDemo.Tests;
+
+public class BaseTestExampleTests : BaseTest
+{
+    [Test]
+    public void SauceDemo_OpensUsingBaseTest()
+    {
+        Driver.Navigate().GoToUrl(
+            "https://www.saucedemo.com"
+        );
+
+        Assert.That(
+            Driver.Title,
+            Does.Contain("Swag Labs")
+        );
+    }
+}
