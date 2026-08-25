28f64b35c7ee938785f6471b9a746c2ff6e2fbb8
Maria <115981159+vcmeamaria@users.noreply.github.com>
2026-08-25 10:03:46 +0100

    Add cross-browser Driver Factory

---
+++ SeleniumCSharpDemo/Drivers/DriverFactory.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumCSharpDemo.Drivers;
+
+public static class DriverFactory
+{
+    public static IWebDriver CreateDriver(string browser)
+    {
+        return browser.ToLower() switch
+        {
+            "chrome" => new ChromeDriver(),
+            "edge" => new EdgeDriver(),
+            "firefox" => new FirefoxDriver(),
+            _ => throw new ArgumentException("Unsupported browser")
+        };
+    }
+}
---
+++ SeleniumCSharpDemo/Tests/DriverFactoryTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using SeleniumCSharpDemo.Drivers;
+
+namespace SeleniumCSharpDemo.Tests;
+
+public class DriverFactoryTests
+{
+    private IWebDriver driver = null!;
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver?.Quit();
+        driver?.Dispose();
+    }
+
+    [TestCase("chrome")]
+    [TestCase("edge")]
+    [TestCase("firefox")]
+    public void DriverFactory_OpensSauceDemo(string browser)
+    {
+        driver = DriverFactory.CreateDriver(browser);
+
+        driver.Manage().Window.Maximize();
+
+        driver.Navigate().GoToUrl("https://www.saucedemo.com");
+
+        Assert.That(
+            driver.Title,
+            Does.Contain("Swag Labs")
+        );
+    }
+}
