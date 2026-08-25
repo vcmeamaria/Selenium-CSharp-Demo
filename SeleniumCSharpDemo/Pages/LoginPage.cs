2a5b566ab58e9de9e9ffcbe0e49b2b4d802c3bbb
Maria <115981159+vcmeamaria@users.noreply.github.com>
2026-08-25 09:18:49 +0100

    Add Page Object Model login page

---
+++ SeleniumCSharpDemo/Pages/LoginPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumCSharpDemo.Pages;
+
+public class LoginPage
+{
+    private readonly IWebDriver driver;
+
+    public LoginPage(IWebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    private IWebElement Username =>
+        driver.FindElement(By.Id("user-name"));
+
+    private IWebElement Password =>
+        driver.FindElement(By.Id("password"));
+
+    private IWebElement LoginButton =>
+        driver.FindElement(By.Id("login-button"));
+
+    public void Login(string username, string password)
+    {
+        Username.SendKeys(username);
+        Password.SendKeys(password);
+        LoginButton.Click();
+    }
+}
