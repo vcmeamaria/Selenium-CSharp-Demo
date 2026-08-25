c02b3a06413f4e899a3d4888b101cf7158e9ee56
Maria <115981159+vcmeamaria@users.noreply.github.com>
2026-08-25 09:43:47 +0100

    Add ChromeOptions Selenium test

---
+++ SeleniumCSharpDemo/Tests/ChromeOptionsTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCSharpDemo.Tests;
+
+public class ChromeOptionsTests
+{
+    private IWebDriver driver = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        ChromeOptions options = new ChromeOptions();
+
+        options.AddArgument("--start-maximized");
+        options.AddArgument("--disable-notifications");
+
+        driver = new ChromeDriver(options);
+    }
+
+    [Test]
+    public void ChromeOptions_OpenSauceDemo()
+    {
+        driver.Navigate().GoToUrl("https://www.saucedemo.com");
+
+        Assert.That(
+            driver.Title,
+            Does.Contain("Swag Labs")
+        );
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver?.Quit();
+        driver?.Dispose();
+    }
+}
