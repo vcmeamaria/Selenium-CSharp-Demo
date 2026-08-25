5c9cf124fcb5d8bf9e5b517349c7e0ff1ecff8d5
Maria <115981159+vcmeamaria@users.noreply.github.com>
2026-08-25 09:25:20 +0100

    Add headless Selenium test

---
+++ SeleniumCSharpDemo/Tests/HeadlessTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCSharpDemo.Tests;
+
+public class HeadlessTests
+{
+    private IWebDriver driver = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        ChromeOptions options = new ChromeOptions();
+
+        options.AddArgument("--headless");
+        options.AddArgument("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+    }
+
+    [Test]
+    public void GoogleHomePage_Headless_IsDisplayed()
+    {
+        driver.Navigate().GoToUrl("https://www.google.com");
+
+        Assert.That(
+            driver.Title,
+            Does.Contain("Google")
+        );
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver?.Quit();
+        driver?.Dispose();
+    }
+}
