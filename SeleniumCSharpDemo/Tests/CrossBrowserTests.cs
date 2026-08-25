b230f40dba09077b7c7505de3e25dc0684afe57e
Maria <115981159+vcmeamaria@users.noreply.github.com>
2026-08-25 09:54:44 +0100

    Add Edge and Firefox cross-browser tests

---
+++ SeleniumCSharpDemo/Tests/CrossBrowserTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumCSharpDemo.Tests;
+
+public class CrossBrowserTests
+{
+    [Test]
+    public void SauceDemo_OpensInEdge()
+    {
+        IWebDriver driver = new EdgeDriver();
+
+        try
+        {
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://www.saucedemo.com");
+
+            Assert.That(
+                driver.Title,
+                Does.Contain("Swag Labs")
+            );
+        }
+        finally
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+
+    [Test]
+    public void SauceDemo_OpensInFirefox()
+    {
+        IWebDriver driver = new FirefoxDriver();
+
+        try
+        {
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://www.saucedemo.com");
+
+            Assert.That(
+                driver.Title,
+                Does.Contain("Swag Labs")
+            );
+        }
+        finally
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
